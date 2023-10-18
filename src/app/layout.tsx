45da87f6ec6817381f981fec50d81f6a1cae7741
arth <arth@globaltllc.com>
2023-10-19 07:20:17 +0800

    change favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from "next";
 import { Signika } from "next/font/google";
 import "slick-carousel/slick/slick-theme.css";
 import "slick-carousel/slick/slick.css";
+import FaviconMetadata from "./FaviconMetadata";
 import SessionProvider from "./SessionProvider";
 import "./globals.css";
 
@@ -21,6 +22,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <FaviconMetadata />
       <body className={signika.className}>
         <SessionProvider>
           <Navbar />
