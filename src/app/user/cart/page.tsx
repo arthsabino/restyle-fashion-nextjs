4a442d9b4c6358bbddfae9bbbe4acd03c67bf8b7
arth <arth@globaltllc.com>
2023-10-23 21:50:59 +0800

    fix in session

--- src/app/user/cart/page.tsx
+++ src/app/user/cart/page.tsx
@@ -1,6 +1,8 @@
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import NoData from "@/components/NoData";
 import { getCart } from "@/lib/db/cart";
 import { Metadata } from "next";
+import { getServerSession } from "next-auth";
 import CartDetails from "./CartDetails";
 import CheckoutDetails from "./CheckoutDetails";
 import ShopNowButton from "./ShopNowButton";
@@ -11,6 +13,7 @@ export const metadata: Metadata = {
 
 export default async function CartPage() {
   const cart = await getCart();
+  const session = await getServerSession(authOptions);
   return (
     <div className="content-container py-4 pb-8">
       <h1 className="text-4xl text-center">My Cart</h1>
@@ -18,7 +21,11 @@ export default async function CartPage() {
       {cart && cart.items.length > 0 ? (
         <div className="flex gap-x-4 gap-y-6 mt-4 md:flex-row flex-col">
           <CartDetails className="basis-2/3" />
-          <CheckoutDetails className="basis-1/3" cart={cart} />
+          <CheckoutDetails
+            className="basis-1/3"
+            cart={cart}
+            session={session}
+          />
         </div>
       ) : (
         <div className="flex justify-center flex-col items-center gap-4">
