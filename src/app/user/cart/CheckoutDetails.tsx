2ff87394b4c04f0b8c8d615d5c7f4f79ad72d224
arth <arth@globaltllc.com>
2023-10-23 21:47:26 +0800

    fix in import

--- src/app/user/cart/CheckoutDetails.tsx
+++ src/app/user/cart/CheckoutDetails.tsx
@@ -4,7 +4,7 @@ import { ShoppingCart } from "@/lib/db/cart";
 import { createOrderFromCart } from "@/lib/db/order";
 import { formatPrice } from "@/lib/format";
 import { Session } from "next-auth";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import { useTransition } from "react";
 import { toast } from "react-toastify";
 import { twMerge } from "tailwind-merge";
